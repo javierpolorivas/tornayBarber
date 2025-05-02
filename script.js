88a5150df72dfed4e8cd86ef8ac1b8d61f9bc85f
Javier Polo <javierpolorvs@gmail.com>
2025-05-02 14:44:30 +0200

    Tercer commit

--- script.js
+++ script.js
@@ -30,37 +30,3 @@ document.querySelector('.close-btn').addEventListener('click', function() {
 document.querySelector('.hamburger').addEventListener('click', function() {
   document.querySelector('.sidebar').classList.add('active');
 });
-
-
-// GALERIA
-document.addEventListener('DOMContentLoaded', function() {
-  const jsonFile = '/json/gallery.json';
-  
-  const galleryContainer = document.getElementById('gallery-container');
-  
-  fetch(jsonFile)
-      .then(response => response.json())
-      .then(data => {
-          data.forEach(item => {
-              const galleryItem = document.createElement('div');
-              galleryItem.classList.add('gallery-item');
-              
-              const image = document.createElement('img');
-              image.src = item.image;
-              
-              const title = document.createElement('div');
-              
-              galleryItem.appendChild(image);
-              
-              galleryContainer.appendChild(galleryItem);
-          });
-      })
-      .catch(error => {
-          console.error('Error al cargar las imágenes:', error);
-      });
-});
-
-
-
-
-
